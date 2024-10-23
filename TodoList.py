5f82706fe415cb641dc90ff05842a8d0eb28fb3b
tuxiantian <tuxiantian@163.com>
2024-10-23 21:54:40 +0800

    查询已结束的todos

--- TodoList.py
+++ TodoList.py
@@ -44,6 +44,12 @@ def create_todo():
 # 获取待办事项列表
 @todolist_bp.route('/todos', methods=['GET'])
 def get_todos():
+    now = dt.utcnow()
+    expired_todos = TodoItem.query.filter(TodoItem.end_time < now, TodoItem.status != 'ended').all()
+    for todo in expired_todos:
+        todo.status = 'ended'
+    db.session.commit()  # 更新数据库
+
     todos = TodoItem.query.all()
     todos_data = []
     for todo in todos:
@@ -99,6 +105,35 @@ def get_completed_todos():
     
     # 使用分页获取已完成的待办事项，按更新时间倒序排列
     completed_todos_query = TodoItem.query.filter_by(status='completed').order_by(TodoItem.updated_at.desc())
+    
+    # 实现分页
+    pagination = completed_todos_query.paginate(page=page,
+        per_page=page_size,
+        error_out=False)
+
+    results = [{
+        'id': todo.id,
+        'name': todo.name,
+        'start_time': todo.start_time,
+        'end_time': todo.end_time,
+        'importance': todo.importance,
+        'urgency': todo.urgency,
+        'updated_at': todo.updated_at
+    } for todo in pagination.items]
+
+    return jsonify({
+        'todos': results,
+        'total_pages': pagination.pages,
+        'current_page': pagination.page
+    }), 200
+
+@todolist_bp.route('/todos/ended', methods=['GET'])
+def get_ended_todos():
+    page = request.args.get('page', 1, type=int)
+    page_size = request.args.get('page_size', 10, type=int)
+    
+    # 使用分页获取已完成的待办事项，按更新时间倒序排列
+    completed_todos_query = TodoItem.query.filter_by(status='ended').order_by(TodoItem.updated_at.desc())
     
     # 实现分页
     pagination = completed_todos_query.paginate(page=page,
