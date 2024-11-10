7b80d37e6c581c29b75a23a7e78cda82d5ea3dfa
tuxiantian <tuxiantian@163.com>
2024-11-10 19:04:29 +0800

    平衡决策分页

--- BalancedDecision.py
+++ BalancedDecision.py
@@ -1,5 +1,6 @@
 import json
 from flask import Flask, request, jsonify, Blueprint
+from sqlalchemy import desc
 from shared_models import BalancedDecision, db
 from datetime import datetime as dt
 from flask_login import current_user
@@ -39,7 +40,9 @@ def save_decision():
 # 获取所有决策数据的API接口
 @balanced_decision_bp.route('/api/get_decisions', methods=['GET'])
 def get_decisions():
-    decisions = BalancedDecision.query.filter_by(user_id=current_user.id).all()
+    page = request.args.get('page', 1, type=int)
+    page_size = request.args.get('page_size', 10, type=int)
+    decisions = BalancedDecision.query.filter_by(user_id=current_user.id).order_by(desc(BalancedDecision.created_at)).paginate(page=page, per_page=page_size, error_out=False)
     decisions_list = [
         {
             'id': decision.id,
@@ -48,7 +51,12 @@ def get_decisions():
             'created_at': decision.created_at
         } for decision in decisions
     ]
-    return jsonify(decisions_list)
+    return jsonify({
+        'decisions_list': decisions_list,
+        'total_pages': decisions.pages,
+        'current_page': decisions.page,
+        'total_items': decisions.total
+    }), 200
 
 # 获取单个决策详情的API接口
 @balanced_decision_bp.route('/api/get_decision/<int:id>', methods=['GET'])
